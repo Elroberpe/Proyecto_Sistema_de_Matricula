92588f3bcb59db935120a17a201f127c7b448930
CarMorv <carlosmorenovi2000@gmail.com>
2026-05-25 00:40:15 -0500

    Primer Commit

---
+++ Proyecto_Sistema_De_Matricula/src/GUI/PanelConsultaCursos.java
@@ -0,0 +1,29 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class PanelConsultaCursos extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+
+	public PanelConsultaCursos() {
+		
+		setLayout(new BorderLayout());
+		setBackground(new Color(245, 247, 250));
+
+		JLabel titulo = new JLabel("Módulo de Consultas de los Cursos", SwingConstants.CENTER);
+		titulo.setFont(new Font("Segoe UI", Font.BOLD, 28));
+		titulo.setForeground(new Color(33, 37, 41));
+
+		add(titulo, BorderLayout.CENTER);
+
+	}
+
+}
---
+++ Proyecto_Sistema_De_Matricula/src/GUI/PanelConsultaMatriculas.java
@@ -0,0 +1,28 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class PanelConsultaMatriculas extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public PanelConsultaMatriculas() {
+		
+		setLayout(new BorderLayout());
+		setBackground(new Color(245, 247, 250));
+
+		JLabel titulo = new JLabel("Módulo de los retiros", SwingConstants.CENTER);
+		titulo.setFont(new Font("Segoe UI", Font.BOLD, 28));
+		titulo.setForeground(new Color(33, 37, 41));
+
+		add(titulo, BorderLayout.CENTER);
+
+	}
+
+}
