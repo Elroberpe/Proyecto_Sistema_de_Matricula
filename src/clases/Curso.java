65d976f36ace238322567e5d7a0036fb1d1b91a4
CarMorv <carlosmorenovi2000@gmail.com>
2026-06-22 17:56:10 -0500

    clases completadas

--- src/clases/Curso.java
+++ src/clases/Curso.java
@@ -2,7 +2,64 @@ package clases;
 
 public class Curso {
 
-	private int codCurso, ciclo, creditos, horas;
-	private String asignatura;
-	
+    private int codCurso, ciclo, creditos, horas;
+    private String asignatura;
+
+
+    public Curso() {
+    }
+
+
+    public Curso(int codCurso, String asignatura, int ciclo, int creditos, int horas) {
+        this.codCurso = codCurso;
+        this.asignatura = asignatura;
+        this.ciclo = ciclo;
+        this.creditos = creditos;
+        this.horas = horas;
+    }
+
+
+    public int getCodCurso() {
+        return codCurso;
+    }
+
+    public void setCodCurso(int codCurso) {
+        this.codCurso = codCurso;
+    }
+
+    public int getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public String getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(String asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public String toLineaArchivo() {
+        return codCurso + "," + asignatura + "," + ciclo + "," + creditos + "," + horas;
+    }
 }
